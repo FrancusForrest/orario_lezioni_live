9bf89bbf39bc80f7c727f58fe2675be21d69f999
FrancusForrest <38054215+FrancusForrest@users.noreply.github.com>
2020-04-06 21:29:49 +0200

    Add files via upload

--- orario.sh
+++ orario.sh
@@ -18,9 +18,7 @@ fi
 
 # Scarico file 
 echo "========== Scarico il file aggiornato =========="
-wwget --no-check-certificate --output-document=./orario/orario.xlsx 'https://docs.google.com/spreadsheets/d/1OeVJ688pmtfMRGlFJIySo1CiJSwv4cymvBi71VJG9DY/export?gid=0&format=xlsx'
-echo "========== Converto xlsx to csv =========="
-ssconvert ./orario/orario.xlsx ./orario/orario.csvget --no-check-certificate --output-document=./orario/orario.csv 'https://docs.google.com/spreadsheets/d/1OeVJ688pmtfMRGlFJIySo1CiJSwv4cymvBi71VJG9DY/export?gid=0&format=csv'
+wget --no-check-certificate --output-document=./orario/orario.csv 'https://docs.google.com/spreadsheets/d/1OeVJ688pmtfMRGlFJIySo1CiJSwv4cymvBi71VJG9DY/export?gid=0&format=csv'
 
 # Check se il file orario_filtrato.txt esiste già
 if [[ -f "$FILE_LEZIONI_FILTRATE" ]];then
@@ -50,57 +48,45 @@ done < $FILE_CONF_MATERIE
 # Codici materie
 echo ${codici_materie[@]}
 
-# Per avere data di oggi
-DATA_OGGI=$(date +'%Y/%m/%d')
-echo "Oggi è : $DATA_OGGI"
-
-
-
 ###############################################################################################
 IFS=","
 while read f1 f2 f3 f4 f5 f6 f7 f8 f9 f10
 do
 	for r in ${riga_file_conf[@]}
 	do 
-		if [[ "$f5" == *${codici_materie[$r]}* || "$f6" == *${codici_materie[$r]}* ]]; then 	
+		if [[ "$f4" == *${codici_materie[$r]}* || "$f5" == *${codici_materie[$r]}* ]];then 	
 		echo "======================================================================================================="  >> $FILE_LEZIONI_FILTRATE
 		echo "Informazioni cronologiche : $f1" >> $FILE_LEZIONI_FILTRATE
-		echo "Info non rilevanti : $f2" >> $FILE_LEZIONI_FILTRATE
-		echo "Cognome e nome docente : $f3" >> $FILE_LEZIONI_FILTRATE
-		echo "Titolo Insegnamento : $f4" >> $FILE_LEZIONI_FILTRATE
+		#echo "Info non rilevanti : $f2" >> $FILE_LEZIONI_FILTRATE
+		echo "Cognome e nome docente : $f2" >> $FILE_LEZIONI_FILTRATE
+		echo "Titolo Insegnamento : $f3" >> $FILE_LEZIONI_FILTRATE
+		echo "Codice Insegnamento : $f4" >> $FILE_LEZIONI_FILTRATE
 		echo "Codice Insegnamento : $f5" >> $FILE_LEZIONI_FILTRATE
-		echo "Codice Insegnamento : $f6" >> $FILE_LEZIONI_FILTRATE
-		echo "Data lezione live : $f7" >> $FILE_LEZIONI_FILTRATE
-		echo "Ora inizio lezione live : $f8" >> $FILE_LEZIONI_FILTRATE
-		echo "Ora fine lezione live : $f9" >> $FILE_LEZIONI_FILTRATE
-		echo "Collegamento sessione live da copiare e incollare da interfaccia di gestione Microsoft Teams : $f10" >> $FILE_LEZIONI_FILTRATE
+		echo "Data lezione live : $f6" >> $FILE_LEZIONI_FILTRATE
+		echo "Ora inizio lezione live : $f7" >> $FILE_LEZIONI_FILTRATE
+		echo "Ora fine lezione live : $f8" >> $FILE_LEZIONI_FILTRATE
+		echo "Collegamento sessione live da copiare e incollare da interfaccia di gestione Microsoft Teams : $f9" >> $FILE_LEZIONI_FILTRATE
 		echo ""  >> $FILE_LEZIONI_FILTRATE
 
 		# Scrivo file per le lezioni del giorno attuale 
-		if [[ $DATA_OGGI ==  $f7 || $DATA_OGGI ==  $f8  ]]; then
+		if [[ "$f6" == $(date +'%d/%m/%Y') || "$f7" == $(date +'%d/%m/%Y') ]];then
 			echo "======================================================================================================="  >> $FILE_LEZIONI_OGGI
 			echo "Informazioni cronologiche : $f1" >> $FILE_LEZIONI_OGGI
-			echo "Info non rilevanti : $f2" >> $FILE_LEZIONI_OGGI
-			echo "Cognome e nome docente : $f3" >> $FILE_LEZIONI_OGGI
-			echo "Titolo Insegnamento : $f4" >> $FILE_LEZIONI_OGGI
+			#echo "Info non rilevanti : $f2" >> $FILE_LEZIONI_OGGI
+			echo "Cognome e nome docente : $f2" >> $FILE_LEZIONI_OGGI
+			echo "Titolo Insegnamento : $f3" >> $FILE_LEZIONI_OGGI
+			echo "Codice Insegnamento : $f4" >> $FILE_LEZIONI_OGGI
 			echo "Codice Insegnamento : $f5" >> $FILE_LEZIONI_OGGI
-			echo "Codice Insegnamento : $f6" >> $FILE_LEZIONI_OGGI
-			echo "Data lezione live : $f7" >> $FILE_LEZIONI_OGGI
-			echo "Ora inizio lezione live : $f8" >> $FILE_LEZIONI_OGGI
-			echo "Ora fine lezione live : $f9" >> $FILE_LEZIONI_OGGI
-			echo "Collegamento sessione live da copiare e incollare da interfaccia di gestione Microsoft Teams : $f10" >> $FILE_LEZIONI_OGGI
+			echo "Data lezione live : $f6" >> $FILE_LEZIONI_OGGI
+			echo "Ora inizio lezione live : $f7" >> $FILE_LEZIONI_OGGI
+			echo "Ora fine lezione live : $f8" >> $FILE_LEZIONI_OGGI
+			echo "Collegamento sessione live da copiare e incollare da interfaccia di gestione Microsoft Teams : $f9" >> $FILE_LEZIONI_OGGI
 			echo ""  >> $FILE_LEZIONI_OGGI
 		fi
 		fi
 	done
-
-	#echo "$f5"
 	
 done < ./orario/orario.csv
 
-echo "========== Guarda $FILE =========="
-echo "========== Converto file .txt -> .pdf =========="
-cupsfilter ./risultati/orario_filtrato.txt > ./risultati/orario_filtrato.pdf
-cupsfilter ./risultati/lezioni_oggi.txt > ./risultati/lezioni_oggi.pdf
 echo "========== OPERAZIONI TERMINATE =========="
 
